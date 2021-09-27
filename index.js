74fabec4445ccdc94a5c2f567e885bd88d6b4af9
Karuboniru <nico@niconi.org>
2021-09-27 16:12:23 +0800

    handle cache and set a longer max-age for cache

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 });
 
+const cache = caches.default
+
 async function handleRequest(request) {
-  const url = new URL(request.url);
+  const url = new URL(request.url)
   const imageURL = url.searchParams.get('url');
   if(!imageURL) {
     return new Response(null, {status: 206});
   }
+  
+  let response = await cache.match(imageURL)
+  if (!response) {
+    const imageRequest = new Request(imageURL, {headers: request.headers});
+    response = await fetch(imageRequest)
+    responseClone = new Response(response.clone().body,{headers: response.headers})
+    responseClone.headers.set("Cache-Control", "max-age=604800")
+    cache.put(imageURL, responseClone)
+  }
 
-  const imageRequest = new Request(imageURL, {headers: request.headers});
-  return fetch(imageRequest);
+  return response
 }
